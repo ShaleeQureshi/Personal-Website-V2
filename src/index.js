fed676733458bda2ab10aab41eb5458140e79155
Shalee Qureshi <shaleequreshi2019@gmail.com>
2020-11-28 21:10:19 -0500

    Changes to be committed:

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom";
+import App from "./scripts/App";
+
+// Importing styles
+import "./styles/fonts.css";
+import "./styles/text.scss";
+import "./styles/spacing.css";
+import "./styles/responsive.scss";
+import "./styles/main.scss";
+
+ReactDOM.render(<App />, document.getElementById("root"));
